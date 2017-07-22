efa7bc75c57ceb3eaab6c9b7f37ee80aff633a9a
Alex <aamlima123@gmail.com>
2017-07-22 16:49:30 -0300

    mouse.py code cleanup

--- mouse.py
+++ mouse.py
@@ -20,41 +20,53 @@ MOUSEEVENTF_WHEEL = 0x0800
 MOUSEEVENTF_XDOWN = 0x0080
 MOUSEEVENTF_XUP = 0x0100
 
+MOUSE_ABSOLUTE_MAX = 65535  # 0xFFFF
 
-def __mouse_click(flag):
-    mouse_input = INPUT(INPUT_MOUSE, mi=MOUSEINPUT(
-        dwFlags=flag))
-    SENDINPUT(1, byref(mouse_input), sizeof(INPUT))
+
+def mouse_event(flags):
+    mouse_inputs = (INPUT * len(flags))()
+    for i, flag in enumerate(flags):
+        mouse_inputs[i] = INPUT(INPUT_MOUSE, mi=MOUSEINPUT(dwFlags=flag))
+
+    return SENDINPUT(len(flags), mouse_inputs, sizeof(INPUT))
 
 
 def mouse_move(input_x, input_y, absolute=False):
     mouse_input = INPUT(INPUT_MOUSE, mi=MOUSEINPUT(
-        input_x, input_y, dwFlags=MOUSEEVENTF_MOVE + (MOUSEEVENTF_ABSOLUTE if absolute else 0x0)))
-    SENDINPUT(1, byref(mouse_input), sizeof(INPUT))
+        input_x, input_y, dwFlags=MOUSEEVENTF_MOVE + (MOUSEEVENTF_ABSOLUTE if absolute else 0)))
+
+    return SENDINPUT(1, byref(mouse_input), sizeof(INPUT))
 
 
 def mouse_wheel_move(movement, horizontal=False):
     mouse_input = INPUT(INPUT_MOUSE, mi=MOUSEINPUT(
         mouseData=movement, dwFlags=MOUSEEVENTF_HWHEEL if horizontal else MOUSEEVENTF_WHEEL))
-    SENDINPUT(1, byref(mouse_input), sizeof(INPUT))
+
+    return SENDINPUT(1, byref(mouse_input), sizeof(INPUT))
 
 
 def mouse_left_click(repeats=1, delay=0):
+    count = 0
     for _ in range(0, repeats):
-        __mouse_click(MOUSEEVENTF_LEFTDOWN)
-        __mouse_click(MOUSEEVENTF_LEFTUP)
+        count += mouse_event([MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP])
         sleep(delay)
+
+    return count
 
 
 def mouse_right_click(repeats=1, delay=0):
+    count = 0
     for _ in range(0, repeats):
-        __mouse_click(MOUSEEVENTF_RIGHTDOWN)
-        __mouse_click(MOUSEEVENTF_RIGHTUP)
+        count += mouse_event([MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP])
         sleep(delay)
+
+    return count
 
 
 def mouse_middle_click(repeats=1, delay=0):
+    count = 0
     for _ in range(0, repeats):
-        __mouse_click(MOUSEEVENTF_MIDDLEDOWN)
-        __mouse_click(MOUSEEVENTF_MIDDLEUP)
+        count += mouse_event([MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP])
         sleep(delay)
+
+    return count
