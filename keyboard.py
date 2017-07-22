17ef0a29e0b55ea86f20ed9a1b140f24516a0813
Alex <aamlima123@gmail.com>
2017-07-22 13:08:16 -0300

    kbd functions now uses a list of vkcodes as input

--- keyboard.py
+++ keyboard.py
@@ -8,20 +8,27 @@ KEYEVENTF_SCANCODE = 0x0008
 KEYEVENTF_UNICODE = 0x0004
 
 
-def keyboard_press(vkcode):
-    keyboard_input = INPUT(INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode))
-    SENDINPUT(1, byref(keyboard_input), sizeof(INPUT))
+def keyboard_press(vkcodes):
+    keyboard_inputs = (INPUT * len(vkcodes))()
+    for i, vkcode in enumerate(vkcodes):
+        keyboard_inputs[i] = INPUT(INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode))
+    SENDINPUT(len(vkcodes), keyboard_inputs, sizeof(INPUT))
 
 
-def keyboard_release(vkcode):
-    keyboard_input = INPUT(INPUT_KEYBOARD, ki=KEYBDINPUT(
-        vkcode, dwFlags=KEYEVENTF_KEYUP))
-    SENDINPUT(1, byref(keyboard_input), sizeof(INPUT))
+def keyboard_release(vkcodes):
+    keyboard_inputs = (INPUT * len(vkcodes))()
+    for i, vkcode in enumerate(vkcodes):
+        keyboard_inputs[i] = INPUT(
+            INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode, dwFlags=KEYEVENTF_KEYUP))
+    SENDINPUT(len(vkcodes), keyboard_inputs, sizeof(INPUT))
 
 
-def keyboard_tap(vkcode, repeats=1, delay=0):
-    keyboard_inputs = (INPUT * 2)(INPUT(INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode)), INPUT(
-        INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode, dwFlags=KEYEVENTF_KEYUP)))
+def keyboard_tap(vkcodes, repeats=1, delay=0):
+    keyboard_inputs = (INPUT * (2 * len(vkcodes)))()
+    for i, vkcode in enumerate(vkcodes):
+        keyboard_inputs[(i * 2)] = INPUT(INPUT_KEYBOARD, ki=KEYBDINPUT(vkcode))
+        keyboard_inputs[(i * 2) + 1] = INPUT(INPUT_KEYBOARD,
+                                             ki=KEYBDINPUT(vkcode, dwFlags=KEYEVENTF_KEYUP))
     for _ in range(0, repeats):
-        SENDINPUT(2, keyboard_inputs, sizeof(INPUT))
+        SENDINPUT(2 * len(vkcodes), keyboard_inputs, sizeof(INPUT))
         sleep(delay)
