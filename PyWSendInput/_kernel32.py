14eb9f185926b862ef8e386a4ede0ec9166f783e
Alex <aamlima123@gmail.com>
2017-09-01 13:24:57 -0300

    Added is_process_64bit, open_thread, get_thread_stack, get_modules_from_process

--- PyWSendInput/_kernel32.py
+++ PyWSendInput/_kernel32.py
@@ -1,4 +1,7 @@
-from ctypes import WinDLL, wintypes, c_size_t, c_bool, POINTER, Structure
+from ctypes import WinDLL, wintypes, c_size_t, c_bool, POINTER, Structure, Union
+from platform import architecture
+
+IS64BIT = True if architecture()[0] == '64bit' else False
 
 KERNEL32 = WinDLL('kernel32')
 
@@ -15,6 +18,93 @@ class THREADENTRY32(Structure):
 
     def __repr__(self):
         return '%r(th32ThreadID=%r, th32OwnerProcessID=%r)' % (self.__class__, self.th32ThreadID, self.th32OwnerProcessID)
+
+
+class _LDTBYTES(Structure):
+    _fields_ = [('BaseMid', wintypes.BYTE),
+                ('Flags1', wintypes.BYTE),
+                ('Flags2', wintypes.BYTE),
+                ('BaseHi', wintypes.BYTE)]
+
+
+class _LDTBITS(Structure):
+    _fields_ = [('BaseMid', wintypes.DWORD, 8),
+                ('Type', wintypes.DWORD, 5),
+                ('Dpl', wintypes.DWORD, 2),
+                ('Pres', wintypes.DWORD, 1),
+                ('LimitHi', wintypes.DWORD, 4),
+                ('Sys', wintypes.DWORD, 1),
+                ('Reserved_0', wintypes.DWORD, 1),
+                ('Default_Big', wintypes.DWORD, 1),
+                ('Granularity', wintypes.DWORD, 1),
+                ('BaseHi', wintypes.DWORD, 8)]
+
+
+class HIGHWORD(Union):
+    _fields_ = [('Bytes', _LDTBYTES),
+                ('Bits', _LDTBITS)]
+
+
+class LDTENTRY(Structure):
+    _fields_ = [
+        ('LimitLow', wintypes.WORD),
+        ('BaseLow', wintypes.WORD),
+        ('HighWord', HIGHWORD)]
+
+
+SIZEOF80387REGISTERS = 80
+
+
+class FLOATINGSAVEAREA(Structure):
+    _fields_ = [
+        ('ControlWord', wintypes.DWORD),
+        ('StatusWord', wintypes.DWORD),
+        ('TagWord', wintypes.DWORD),
+        ('ErrorOffset', wintypes.DWORD),
+        ('ErrorSelector', wintypes.DWORD),
+        ('DataOffset', wintypes.DWORD),
+        ('DataSelector', wintypes.DWORD),
+        ('RegisterArea', wintypes.BYTE * SIZEOF80387REGISTERS),
+        ('Cr0NpxState', wintypes.DWORD)
+    ]
+
+
+MAXIMUMSUPPORTEDEXTENSION = 512
+
+
+class CONTEXT(Structure):
+    _fields_ = [
+        ('ContextFlags', wintypes.DWORD),
+        ('Dr0', wintypes.DWORD),
+        ('Dr1', wintypes.DWORD),
+        ('Dr2', wintypes.DWORD),
+        ('Dr3', wintypes.DWORD),
+        ('Dr6', wintypes.DWORD),
+        ('Dr7', wintypes.DWORD),
+        ('FloatSave', FLOATINGSAVEAREA),
+        ('SegGs', wintypes.DWORD),
+        ('SegFs', wintypes.DWORD),
+        ('SegEs', wintypes.DWORD),
+        ('SegDs', wintypes.DWORD),
+        ('Edi', wintypes.DWORD),
+        ('Esi', wintypes.DWORD),
+        ('Ebx', wintypes.DWORD),
+        ('Edx', wintypes.DWORD),
+        ('Ecx', wintypes.DWORD),
+        ('Eax', wintypes.DWORD),
+        ('Ebp', wintypes.DWORD),
+        ('Eip', wintypes.DWORD),
+        ('SegCs', wintypes.DWORD),
+        ('EFlags', wintypes.DWORD),
+        ('Esp', wintypes.DWORD),
+        ('SegSs', wintypes.DWORD),
+        ('ExtendedRegisters', wintypes.BYTE * MAXIMUMSUPPORTEDEXTENSION)]
+
+
+class MODULEINFO(Structure):
+    _fields_ = [('lpBaseOfDll', wintypes.LPVOID),
+                ('SizeOfImage', wintypes.DWORD),
+                ('EntryPoint', wintypes.LPVOID)]
 
 
 # process
@@ -47,4 +137,40 @@ THREAD32FIRST.restype = c_bool
 THREAD32NEXT = KERNEL32.Thread32Next
 THREAD32NEXT.argtypes = [wintypes.HANDLE, POINTER(THREADENTRY32)]
 THREAD32NEXT.restype = c_bool
+
+ISWOW64PROCESS = KERNEL32.IsWow64Process
+ISWOW64PROCESS.argtypes = [wintypes.HANDLE, POINTER(c_bool)]
+ISWOW64PROCESS.restype = c_bool
+
+GETTHREADSELECTORENRTY = KERNEL32.Wow64GetThreadSelectorEntry if IS64BIT else KERNEL32.GetThreadSelectorEntry
+GETTHREADSELECTORENRTY.argtypes = [
+    wintypes.HANDLE, wintypes.DWORD, POINTER(LDTENTRY)]
+GETTHREADSELECTORENRTY.restype = c_bool
+
+GETTHREADCONTEXT = KERNEL32.Wow64GetThreadContext if IS64BIT else KERNEL32.GetThreadContext
+GETTHREADCONTEXT.argtypes = [wintypes.HANDLE, POINTER(CONTEXT)]
+GETTHREADCONTEXT.restype = c_bool
+
+OPENTHREAD = KERNEL32.OpenThread
+OPENTHREAD.argtypes = [wintypes.DWORD, c_bool, wintypes.DWORD]
+OPENTHREAD.restype = wintypes.HANDLE
+
+GETMODULEHANDLE = KERNEL32.GetModuleHandleW
+GETMODULEHANDLE.argtypes = [wintypes.LPCWSTR]
+GETMODULEHANDLE.restype = wintypes.HMODULE
+
+GETMODULEINFORMATION = KERNEL32.K32GetModuleInformation
+GETMODULEINFORMATION.argtypes = [
+    wintypes.HANDLE, wintypes.HMODULE, POINTER(MODULEINFO), wintypes.DWORD]
+GETMODULEINFORMATION.restype = c_bool
+
+ENUMPROCESSMODULESEX = KERNEL32.K32EnumProcessModulesEx
+ENUMPROCESSMODULESEX.argtypes = [wintypes.HANDLE, POINTER(
+    wintypes.HMODULE), wintypes.DWORD, POINTER(wintypes.DWORD), wintypes.DWORD]
+ENUMPROCESSMODULESEX.restype = c_bool
+
+GETMODULEFILENAMEEX = KERNEL32.K32GetModuleFileNameExW
+GETMODULEFILENAMEEX.argtypes = [
+    wintypes.HANDLE, wintypes.HMODULE, wintypes.LPWSTR, wintypes.DWORD]
+GETMODULEFILENAMEEX.restype = wintypes.DWORD
 # endprocess
--- PyWSendInput/process.py
+++ PyWSendInput/process.py
@@ -1,14 +1,31 @@
 from copy import copy
-from ctypes import byref, sizeof, wintypes
+from ctypes import byref, c_bool, c_int, sizeof, wintypes
+from os import path
 
-from PyWSendInput._kernel32 import (CLOSEHANDLE, CREATETOOLHELP32SNAPSHOT,
-                                    OPENPROCESS, READPROCESSMEMORY,
+from PyWSendInput._kernel32 import (CLOSEHANDLE, CONTEXT,
+                                    CREATETOOLHELP32SNAPSHOT,
+                                    ENUMPROCESSMODULESEX, GETMODULEFILENAMEEX,
+                                    GETMODULEINFORMATION, GETTHREADCONTEXT,
+                                    GETTHREADSELECTORENRTY, IS64BIT,
+                                    ISWOW64PROCESS, LDTENTRY, MODULEINFO,
+                                    OPENPROCESS, OPENTHREAD, READPROCESSMEMORY,
                                     THREAD32FIRST, THREAD32NEXT, THREADENTRY32,
                                     WRITEPROCESSMEMORY)
 from PyWSendInput._user32 import GETWINDOWTHREADPROCESSID
 
 PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
+
 TH32CS_SNAPALL = (0x00000001 | 0x00000002 | 0x00000004 | 0x00000008)
+
+THREAD_GET_CONTEXT = 0x0008
+THREAD_QUERY_INFORMATION = 0x0040
+
+CONTEXT_PLATFORM = 0x00100000 if IS64BIT else 0x00010000
+CONTEXT_SEGMENTS = (CONTEXT_PLATFORM | 0x0004)
+
+LIST_MODULES_32BIT = 0x01
+LIST_MODULES_64BIT = 0x02
+LIST_MODULES_ALL = 0x03
 
 
 def get_window_thread_process_id(window, process_id=None):
@@ -37,7 +54,7 @@ def close_handle(process):
     return CLOSEHANDLE(process)
 
 
-def get_threads_from_process(pid=None):
+def get_threads_from_process_id(pid=None):
     thread_snap = CREATETOOLHELP32SNAPSHOT(TH32CS_SNAPALL, 0)
     threads = []
 
@@ -60,3 +77,96 @@ def get_threads_from_process(pid=None):
     close_handle(thread_snap)
 
     return threads
+
+
+def is_process_64bit(phandle):
+    if IS64BIT:
+        iswow64 = c_bool()
+        if ISWOW64PROCESS(phandle, byref(iswow64)):
+            return not iswow64
+        return False
+
+    return False
+
+
+def open_thread(thid, desired_access=THREAD_GET_CONTEXT | THREAD_QUERY_INFORMATION, inherit_handle=False):
+    return OPENTHREAD(desired_access, inherit_handle, thid)
+
+
+def get_thread_stack(thid, phandle):
+    stack = c_int(0)
+    thandle = open_thread(thid)
+    result = 0
+
+    if not thandle:
+        return result
+
+    minfo = MODULEINFO()
+    mhandle = get_modules_from_process(phandle).get('KERNEL32.DLL')
+    if (not mhandle) or (not GETMODULEINFORMATION(phandle, mhandle, byref(minfo), sizeof(minfo))):
+        close_handle(thandle)
+        return result
+
+    is64b = is_process_64bit(phandle)
+
+    if is64b == IS64BIT:
+        pass
+    else:
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_SEGMENTS
+        if not GETTHREADCONTEXT(thandle, byref(context)):
+            close_handle(thandle)
+            return result
+
+        ldt = LDTENTRY()
+        if not GETTHREADSELECTORENRTY(thandle, context.SegFs, byref(ldt)):
+            close_handle(thandle)
+            return result
+
+        val1 = ldt.HighWord.Bytes.BaseMid if ldt.HighWord.Bytes.BaseMid >= 0 else 256 + \
+            ldt.HighWord.Bytes.BaseMid
+        val2 = ldt.HighWord.Bytes.BaseHi if ldt.HighWord.Bytes.BaseHi >= 0 else 256 + \
+            ldt.HighWord.Bytes.BaseHi
+        address = ldt.BaseLow + (val1 << 16) + (val2 << 24)
+        read_process_memory(phandle, address + 4, byref(stack), 4)
+
+    if not stack.value:
+        close_handle(thandle)
+        return result
+
+    result = stack.value
+    addrsize = 8 if is64b else 4
+    buffer = (c_int * (4096 // addrsize))()
+
+    if not read_process_memory(phandle, stack.value - 4096, buffer, 4096):
+        close_handle(thandle)
+        return result
+
+    for idx in range(4096 // 4 - 1, -1, -1):
+        value = buffer[idx]
+        if value >= minfo.lpBaseOfDll and value <= minfo.lpBaseOfDll + minfo.SizeOfImage:
+            result = stack.value - 4096 + idx * addrsize
+            break
+
+    close_handle(thandle)
+
+    return result
+
+
+def get_modules_from_process(phandle):
+    modules = {}
+    hmodules = (wintypes.HMODULE * 1024)()
+    cbneeded = wintypes.DWORD()
+
+    if not ENUMPROCESSMODULESEX(phandle, hmodules, sizeof(hmodules), byref(cbneeded), LIST_MODULES_ALL):
+        print('fail enum')
+        return modules
+
+    for idx in range(0, cbneeded.value // sizeof(wintypes.HMODULE)):
+        mname = (wintypes.WCHAR * 1024)()
+        if GETMODULEFILENAMEEX(phandle, hmodules[idx], mname, 1024):
+            modules[path.split(mname.value)[1]] = hmodules[idx]
+        else:
+            modules[idx] = hmodules[idx]
+
+    return modules
